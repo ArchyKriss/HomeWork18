0f75f740e64578b2ce021ff66f21ec6bcedd9dbf
user <git@github.com:ArchyKris/helloWord.git>
2024-10-08 22:04:12 +0300

    initial commit

---
+++ skyproBookHw/src/main/java/pro/sky/skyproBookHw/SkyproBookHwApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproBookHw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproBookHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproBookHwApplication.class, args);
+	}
+
+}
